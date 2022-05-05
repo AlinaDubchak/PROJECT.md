7c310ee9e7bb3bfec384ae93878719b21d9af324
Masha Martyniuk <mmartyniuk004@gmail.com>
2022-05-05 23:13:40 +0300

    removed magic numbers

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 let main = document.querySelector(".main");
 
+const movingCells = 1;
+const fixedCells = 2;
+
 let rows = Array(10).fill(0);
 let field = Array(20).fill(rows);
 
-function draw() {
+function Draw() {
     let fieldHTML = '';
     for (let y = 0; y < field.length; y++) {
         for (let x = 0; x < field[y].length; x++) {
-            if (field[y][x] === 1) {
+            if (field[y][x] === movingCells) {
                 fieldHTML += '<div class="cell movingCell"></div>'
-            } else if (field[y][x] === 2) {
+            } else if (field[y][x] === fixedCells) {
                 fieldHTML += '<div class="cell fixedCell"></div>'
             } else fieldHTML += '<div class="cell"></div>';
         }
     }
     main.innerHTML = fieldHTML;
 }
-draw();
+Draw();
