2117bf4db93feaf7a9d78fa164eaaadaafa02baa
Masha Martyniuk <mmartyniuk004@gmail.com>
2022-05-11 13:35:26 +0300

    added movement of the figures + rewrote the function for collisions

--- script.js
+++ script.js
@@ -8,7 +8,38 @@ const freeCells = 0;
 
 const rows = Array(10).fill(0);
 const field = Array(20).fill(rows);
+/*let field = [
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+];*/
 
+let activePiece = {
+  x: 0,
+  y: 0,
+  blocks: [
+    [0, 1, 0],
+    [1, 1, 1],
+    [0, 0, 0],
+  ],
+}
 
 function draw() {
   let fieldHTML = '';
@@ -25,30 +56,38 @@ function draw() {
   main.innerHTML = fieldHTML;
 }
 
-function canMoveDown() {
+function removePrevPiece() {
   for (let y = 0; y < field.length; y++) {
     for (let x = 0; x < field[y].length; x++) {
-      if (field[y][x] === movingCells) {
-        if (y === field.length - 1 || field[y + 1][x] === fixedCells) {
-          return false;
-        }
-      }
+     if (field[y][x] === movingCells) {
+       field[y][x] = freeCells;
+     }
     }
   }
-  return true;
+ }
+
+function addActivePiece() {
+  removePrevPiece();
+  for (let y = 0; y < activePiece.blocks.length; y++) {
+    for (let x = 0; x < activePiece.blocks[y].length; x++) {
+      if (activePiece.blocks[y][x] === movingCells) {
+        field[activePiece.y + y][activePiece.x + x] = activePiece.blocks[y][x];
+      }
+      
+    }
+  }
 }
 
-function moveDown() {
-  if (canMoveDown()) {
-    for (let y = field.length - 1; y >= 0; y--) {
-      for (let x = 0; x < field[y].length; x++) {
-        if (field[y][x] === movingCells) {
-          field[y + 1][x] = movingCells;
-          field[y][x] = freeCells;
-        }
-      }
-    }
-  } else fixFigure();
+function hasCollisions() {
+  for (let y = 0; y < activePiece.blocks.length; y++) {
+    for (let x = 0; x < activePiece.blocks[y].length; x++) {
+     if (activePiece.blocks[y][x] === movingCells && 
+      (field[activePiece.y + y] === undefined || field[activePiece.y + y][activePiece.x + x] === undefined)) {
+     return true;
+  }
+}
+}
+return false;
 }
 
 function fixFigure() {
@@ -64,8 +103,34 @@ function fixFigure() {
 const speedFigure = 1000;
 draw();
 
+document.onkeydown = event => {
+  switch (event.keyCode) {
+    case 37: activePiece.x -= 1;
+    if(hasCollisions() === true) {
+      activePiece.x += 1;}
+             break;
+    case 39: activePiece.x += 1;
+    if(hasCollisions() === true) {
+      activePiece.x -= 1;}
+             break;
+    case 40: activePiece.y += 1;
+             if(hasCollisions() === true) {
+                activePiece.y -= 1;
+              fixFigure();
+              activePiece.y = 0;
+            }
+             break;
+  }
+    
+    
+  
+  addActivePiece();
+  draw();
+}
+
+
 function startGame() {
-  moveDown();
+  addActivePiece();
   draw();
   setTimeout(startGame, speedFigure);
 }
