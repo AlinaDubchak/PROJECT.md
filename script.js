d227d9f1d7a6fd1688f15d2396bea5ff76c7ba97
AlinaDubchak <alina.dubchak.ev@gmail.com>
2022-05-13 14:44:54 +0300

    corrections

--- script.js
+++ script.js
@@ -5,9 +5,40 @@ const main = document.querySelector('.main');
 const movingCells = 1;
 const fixedCells = 2;
 const freeCells = 0;
+const speedFigure = 1000;
 
-const rows = Array(10).fill(0);
-const field = Array(20).fill(rows);
+const field = [
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+];
+
+const activePiece = {
+  x: 0,
+  y: 0,
+  blocks: [
+    [0, 1, 0],
+    [1, 1, 1],
+    [0, 0, 0],
+  ],
+};
 
 
 function draw() {
@@ -25,31 +56,50 @@ function draw() {
   main.innerHTML = fieldHTML;
 }
 
-function canMoveDown() {
+function removePrevPiece() {
   for (let y = 0; y < field.length; y++) {
     for (let x = 0; x < field[y].length; x++) {
       if (field[y][x] === movingCells) {
-        if (y === field.length - 1 || field[y + 1][x] === fixedCells) {
-          return false;
-        }
+        field[y][x] = freeCells;
       }
     }
   }
-  return true;
 }
 
-function moveDown() {
-  if (canMoveDown()) {
-    for (let y = field.length - 1; y >= 0; y--) {
-      for (let x = 0; x < field[y].length; x++) {
-        if (field[y][x] === movingCells) {
-          field[y + 1][x] = movingCells;
-          field[y][x] = freeCells;
-        }
+
+let { x: pieceY, x: pieceX } = activePiece;
+const { blocks } = activePiece;
+
+
+function addActivePiece() {
+  removePrevPiece();
+  for (let y = 0; y < blocks.length; y++) {
+    for (let x = 0; x < blocks[y].length; x++) {
+      if (blocks[y][x] === movingCells) {
+        field[pieceY + y][pieceX + x] = blocks[y][x];
       }
     }
-  } else fixFigure();
+  }
 }
+
+
+function hasCollisions() {
+  for (let y = 0; y < blocks.length; y++) {
+    for (let x = 0; x < blocks[y].length; x++) {
+      if (
+        blocks[y][x] &&
+        (field[pieceY + y] === undefined ||
+          field[pieceY + y][pieceX + x] === undefined ||
+          field[pieceY + y][pieceX + x] === fixedCells)
+      ) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+
 
 function fixFigure() {
   for (let y = field.length - 1; y >= 0; y--) {
@@ -61,11 +111,45 @@ function fixFigure() {
   }
 }
 
-const speedFigure = 1000;
-draw();
+function moveDown() {
+  pieceY += 1;
+  if (hasCollisions()) {
+    pieceY -= 1;
+    fixFigure();
+    pieceY = 0;
+  }
+}
+
+document.addEventListener(
+  'keydown',
+  e => {
+    switch (e.code) {
+    case 'ArrowLeft':
+      pieceX -= 1;
+      if (hasCollisions()) {
+        pieceX += 1;
+      }
+      break;
+    case 'ArrowRight':
+      pieceX += 1;
+      if (hasCollisions()) {
+        pieceX -= 1;
+      }
+      break;
+    case 'ArrowDown':
+      moveDown();
+      break;
+    }
+
+    addActivePiece();
+    draw();
+  },
+  true
+);
 
 function startGame() {
   moveDown();
+  addActivePiece();
   draw();
   setTimeout(startGame, speedFigure);
 }
