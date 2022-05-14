0a0bee164e2edba1a6a429a3a1f132d1b46d9e33
AlinaDubchak <alina.dubchak.ev@gmail.com>
2022-05-14 13:14:50 +0300

    random figure

--- script.js
+++ script.js
@@ -30,15 +30,71 @@ const field = [
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 ];
 
-const activePiece = {
-  x: 0,
-  y: 0,
-  blocks: [
-    [0, 1, 0],
-    [1, 1, 1],
-    [0, 0, 0],
-  ],
-};
+let activePiece = createPieces();
+let nextPiece = createPieces();
+
+function createPieces() {
+  const index = Math.floor(Math.random() * 7);
+  const type = 'IJLOTSZ'[index];
+  const piece = { x: 0, y: 0 };
+  switch (type) {
+  case 'I':
+      piece.blocks = [
+        [0, 0, 0, 0],
+        [1, 1, 1, 1],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+      ];
+      break;
+    case 'J':
+      piece.blocks = [
+        [0, 0, 0],
+        [1, 1, 1],
+        [0, 0, 1],
+      ];
+      break;
+    case 'L':
+      piece.blocks = [
+        [0, 0, 0],
+        [1, 1, 1],
+        [1, 0, 0],
+      ];
+      break;
+    case 'O':
+      piece.blocks = [
+        [1, 1],
+        [1, 1],
+      ];
+      break;
+    case 'T':
+      piece.blocks = [
+        [0, 0, 0],
+        [1, 1, 1],
+        [0, 1, 0],
+      ];
+      break;
+    case 'S':
+      piece.blocks = [
+        [0, 0, 0],
+        [0, 1, 1],
+        [1, 1, 0],
+      ];
+      break;
+    case 'Z':
+      piece.blocks = [
+        [0, 0, 0],
+        [1, 1, 0],
+        [0, 1, 1],
+      ];
+      break;
+    default:
+      throw new Error('undefined figure');
+  }
+  piece.x = Math.floor((10 - piece.blocks[0].length) / 2);
+  piece.y = 0;
+
+  return piece;
+}
 
 function draw() {
   let fieldHTML = '';
@@ -110,6 +166,7 @@ function moveDown() {
   if (hasCollisions()) {
     pieceY -= 1;
     fixFigure();
+    updatePiece();
     pieceY = 0;
   }
 }
@@ -118,21 +175,21 @@ document.addEventListener(
   'keydown',
   e => {
     switch (e.code) {
-    case 'ArrowLeft':
-      pieceX -= 1;
-      if (hasCollisions()) {
-        pieceX += 1;
-      }
-      break;
-    case 'ArrowRight':
-      pieceX += 1;
-      if (hasCollisions()) {
+      case 'ArrowLeft':
         pieceX -= 1;
-      }
-      break;
-    case 'ArrowDown':
-      moveDown();
-      break;
+        if (hasCollisions()) {
+          pieceX += 1;
+        }
+        break;
+      case 'ArrowRight':
+        pieceX += 1;
+        if (hasCollisions()) {
+          pieceX -= 1;
+        }
+        break;
+      case 'ArrowDown':
+        moveDown();
+        break;
     }
 
     addActivePiece();
@@ -149,3 +206,8 @@ function startGame() {
 }
 
 setTimeout(startGame, speedFigure);
+
+function updatePiece() {
+  activePiece = nextPiece;
+  nextPiece = createPieces();
+}
